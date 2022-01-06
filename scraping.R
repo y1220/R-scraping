feb5e049da23a7cb47a67932bc19adce67ee9b18
y1220-trial\y1220 <ga.2.y1220@gmail.com>
2022-01-06 21:24:37 +0100

    read html, xml_structure

---
+++ scraping.R
@@ -0,0 +1,16 @@
+library(rvest)
+library(xml2)
+html_excerpt_raw <- '
+<html> 
+  <body> 
+    <h1>Web scraping is cool</h1>
+    <p>It involves writing code – be it R or Python.</p>
+    <p><a href="https://datacamp.com">DataCamp</a> 
+		has courses on it.</p>
+  </body> 
+</html>'
+# Turn the raw excerpt into an HTML document R understands
+html_excerpt <- read_html(html_excerpt_raw)
+html_excerpt
+# Print the HTML excerpt with the xml_structure() function
+xml_structure(html_excerpt)
