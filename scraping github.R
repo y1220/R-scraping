a727fb659b5fe36d35634de24e5cd852c683a378
y1220-trial\y1220 <ga.2.y1220@gmail.com>
2022-01-06 22:50:41 +0100

    html_attr, html_text

---
+++ scraping github.R
@@ -0,0 +1,51 @@
+library(rvest)
+
+read_html("https://github.com/tidyverse/ggplot2") %>%
+  html_nodes(".user-mention") %>% 
+  html_text()
+
+authors <- read_html("https://github.com/tidyverse/ggplot2/commits/main") %>%
+  html_nodes(".user-mention") %>% 
+  html_text()
+
+titles <- read_html("https://github.com/tidyverse/ggplot2/commits/main") %>%
+  html_nodes(".markdown-title") %>% 
+  html_text()
+
+days <- read_html("https://github.com/tidyverse/ggplot2/commits/main") %>%
+  html_nodes("relative-time") %>% 
+  html_text()
+
+# return set of author and date of commit
+
+library(tidyverse)
+link_df <- tibble(
+  domain = authors,
+  name = days
+)
+
+title <- paste(titles[1], titles[2], titles[3])
+title
+
+refined_titles <- tille
+for(i in 1:32) {
+  # i-th element of `u1` squared into `i`-th position of `usq`
+  tmp <- paste(titles[3*i+1], titles[3*i+2], titles[3*i+3])
+  print(tmp)
+  refined_titles <- c(refined_titles, tmp)
+}
+
+# Create a vector filled with random normal values
+u1 <- rnorm(30)
+print("This loop calculates the square of the first 10 elements of vector u1")
+
+# Initialize `usq`
+usq <- 0
+
+for(i in 1:10) {
+  # i-th element of `u1` squared into `i`-th position of `usq`
+  usq[i] <- u1[i]*u1[i]
+  print(usq[i])
+}
+
+print(i)
--- scraping.R
+++ scraping.R
@@ -25,5 +25,25 @@ ol_node <- list_html %>%
 ol_node <- list_html %>% html_node('ol')
 html_children(ol_node)
 
+
 # trial to see content in tidy way
 xml_structure(list_html)
+
+hyperlink_raw_html <- "\n<html>\n  <body>\n    <h3>Helpful links</h3>\n    <ul>\n      <li><a href=\"https://wikipedia.org\">Wikipedia</a></li>\n      <li><a href=\"https://dictionary.com\">Dictionary</a></li>\n      <li><a href=\"https://duckduckgo.com\">Search Engine</a></li>\n    </ul>\n    <small>\n      Compiled with help from <a href=\"https://google.com\">Google</a>.\n    </small>\n  </body>\n</html>"
+# Extract all the a nodes from the bulleted list
+links <- hyperlink_raw_html %>% 
+  read_html() %>%
+  html_nodes('li a') # 'ul a' is also correct!
+
+# Extract the needed values for the data frame
+domain_value = links %>% html_attr('href')
+name_value = links %>% html_text()
+
+# Construct a data frame
+library(tidyverse)
+link_df <- tibble(
+  domain = domain_value,
+  name = name_value
+)
+
+link_df
